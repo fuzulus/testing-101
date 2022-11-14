9c9ba6c15227ac01582c8f8bc713211cadf9de9d
Mateo Fuzul <mfuzul@hotmail.com>
2022-11-14 21:15:48 +0100

    feat: initial setup

---
+++ src/Infrastructure/Driven/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Driven\EventSubscriber;
+
+use App\Infrastructure\Driven\JsonApi\ErrorHandler;
+use DomainException;
+use InvalidArgumentException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Undabot\SymfonyJsonApi\Exception\ParamConverterInvalidUuidFormatException;
+use Undabot\SymfonyJsonApi\Http\Model\Response\JsonApiHttpResponse;
+
+final class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private ErrorHandler $errorHandler)
+    {
+    }
+
+    /** @return array<string, string> */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'buildErrorResponse',
+        ];
+    }
+
+    public function buildErrorResponse(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        $response = match (true) {
+            $exception instanceof ParamConverterInvalidUuidFormatException,
+                $exception instanceof NotFoundHttpException,
+            => JsonApiHttpResponse::notFound(),
+            $exception instanceof AccessDeniedHttpException,
+            => JsonApiHttpResponse::forbidden($this->errorHandler->buildError($exception)),
+            $exception instanceof DomainException,
+                $exception instanceof InvalidArgumentException,
+            => JsonApiHttpResponse::badRequest($this->errorHandler->buildError($exception)),
+            default => null,
+        };
+
+        if (null !== $response) {
+            $event->setResponse($response);
+        }
+    }
+}
---
+++ src/Infrastructure/Driven/JsonApi/ErrorHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Driven\JsonApi;
+
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Throwable;
+use Undabot\JsonApi\Definition\Encoding\DocumentToPhpArrayEncoderInterface;
+use Undabot\JsonApi\Implementation\Model\Document\Document;
+use Undabot\JsonApi\Implementation\Model\Error\Error;
+use Undabot\JsonApi\Implementation\Model\Error\ErrorCollection;
+
+final class ErrorHandler
+{
+    public function __construct(
+        private readonly DocumentToPhpArrayEncoderInterface $documentToPhpArrayEncoder,
+    ) {
+    }
+
+    /** @return mixed[] */
+    public function buildError(Throwable $exception): array
+    {
+        $errorCollection = new ErrorCollection([
+            $this->buildErrorFromException($exception),
+        ]);
+        $document = new Document(null, $errorCollection);
+
+        return $this->documentToPhpArrayEncoder->encode($document);
+    }
+
+    private function buildErrorFromException(Throwable $exception): Error
+    {
+        $class = (new \ReflectionClass($exception))->getShortName();
+
+        $e = FlattenException::createFromThrowable($exception);
+
+        return new Error(
+            null,
+            null,
+            null,
+            (string) $e->getCode(),
+            $e->getMessage(),
+            sprintf(
+                'Exception %s: "%s"',
+                $class,
+                $e->getMessage()
+            )
+        );
+    }
+}
