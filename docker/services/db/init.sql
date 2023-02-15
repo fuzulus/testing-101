7155fa35e2ecfd91c4cf0a5c1162aed6aa2bc9b2
Mateo Fuzul <mfuzul@hotmail.com>
2023-02-15 08:47:01 +0100

    chore(db): create test database and user

--- docker/services/db/init.sql
+++ docker/services/db/init.sql
@@ -1,1 +1,7 @@
-create database test;
+create user 'tester'@'%' identified by 'monalisa123';
+
+create database if not exists test;
+
+grant all privileges on `test`.* to 'tester'@'%' identified by 'monalisa123';
+
+flush privileges;
